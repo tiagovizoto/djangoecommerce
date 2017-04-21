9cfc2bb873f7938cbe7494ef1638729b3a04713c
Tiago Vizoto <vizoto123@gmail.com>
2017-04-21 18:53:34 -0300

    removendo

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -121,3 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
