e5254aef186ac7d83933d82d3f96a50844511012
Tiago Vizoto <vizoto123@gmail.com>
2017-04-26 00:26:53 -0300

    ola

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -147,7 +147,7 @@ ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+#try:
+#    from .local_settings import *
+#except ImportError:
+#    pass
